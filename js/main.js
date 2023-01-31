a51fdfad63319a5ec176323818b7e02aa0f7eb1c
Maciej Sławiński <88053587+endees@users.noreply.github.com>
2023-01-31 14:54:52 +0100

    rwd 50%

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$('.owl-carousel').owlCarousel({
+$('#firstOwl').owlCarousel({
     loop:true,
     margin:10,
     nav:true,
@@ -13,6 +13,24 @@ $('.owl-carousel').owlCarousel({
         },
         1000:{
             items:2
+        }
+    }
+})
+$('#secondOwl').owlCarousel({
+    loop:true,
+    margin:15,
+    nav:false,
+    dots: false,
+    autoplay: 1000,
+    responsive:{
+        0:{
+            items:3
+        },
+        600:{
+            items:5
+        },
+        1000:{
+            items:6
         }
     }
 })
